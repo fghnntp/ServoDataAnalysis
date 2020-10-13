cc273db46d20eea5e350dab0672e13f7c5e857cc
liuly <liuly@MacBook-Pro.local>
2020-10-13 17:51:22 +0800

    添加.pvt选项

--- 文件转换/main.py
+++ 文件转换/main.py
@@ -2,7 +2,7 @@ import sys
 import os 
 from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
 from mainWindow import Ui_transferFile
-from transferFile import set_settings, add_data
+from transferFile import set_settings, add_data, add_data_pvt
 
 class mainWindow(QDialog):
     def __init__(self):
@@ -13,7 +13,8 @@ class mainWindow(QDialog):
         self.filename_src = ""
         self.filename_des = ""
         self.ui.pushButtonSelectFile.clicked.connect(self.open_file)
-        self.ui.pushButtonTransferStaring.clicked.connect(self.transfer_file)
+        self.ui.pushButtonTransferRDI.clicked.connect(self.transfer_file_rdi)
+        self.ui.pushButtonTransferPVT.clicked.connect(self.transfer_file_pvt)
 
     def open_file(self):
         """
@@ -23,13 +24,16 @@ class mainWindow(QDialog):
         self.filename_src = fileName
         self.filename_des = fileName[fileName.rfind("/")+1:fileName.rfind(".")]+".dat"
 
-    def transfer_file(self):
+    def transfer_file_rdi(self):
         """
             对文件进行转换
         """
         set_settings("setting.json", self.filename_des)
         add_data(self.filename_src, self.filename_des)
         print("tranfer "+self.filename_src+" to "+self.filename_des+" complete")
+
+    def transfer_file_pvt(self):
+        add_data_pvt(self.filename_src, self.filename_des[:-4]+'.pvt') 
     
 if __name__ == "__main__":
     app=QApplication(sys.argv)
--- 文件转换/transferFile.py
+++ 文件转换/transferFile.py
@@ -57,16 +57,58 @@ def add_data(file_src, file_des):
     for i in range(len(data_trace_cmda)-1):
         data_trace_cmdj.append(str((eval(data_trace_cmda[i+1])-eval(data_trace_cmda[i]))/0.002))
     data_trace_cmdt = data_trace_t
+    data_trace_cmdtdiff = []
+    for i in range(len(data_trace_t)-1):
+        data_trace_cmdtdiff.append(str(eval(data_trace_t[i+1]-eval(data_trace_cmdt[i]))/0.002))
     with open(file_des, 'a') as f:
-        length = min(len(data_trace_cmdv), len(data_trace_cmda), len(data_trace_cmdj), len(data_trace_cmdt))
+        length = min(len(data_trace_cmdv), len(data_trace_cmda), len(data_trace_cmdj), len(data_trace_cmdt), len(data_trace_cmdtdiff))
         for i in range(length):
-            f.write(data_trace_cmdv[i]+','+data_trace_cmda[i]+','+data_trace_cmdj[i]+","+data_trace_cmdt[i]+'\n')
+            f.write(data_trace_cmdv[i]+','+data_trace_cmda[i]+','+data_trace_cmdj[i]+","+data_trace_cmdt[i]+","+data_trace_cmdtdiff[i]+'\n')
+
+def get_data_pvt(filename, data_trace_v):
+    """
+        get data for pvt
+    """
+    with open(filename, 'r', encoding='gbk') as f:
+        #找到位置指令速度所对应的通道
+        count = 0
+        data_relate = {"data_trace_v":"", "data_trace_t":""}
+        for line in f.readlines():
+            count += 1
+            _ = line[:-1].split(',')
+            if count == 39 or count == 40 or count == 41:
+                if _[-2] == "\"位置指令速度\"":
+                    data_relate["data_trace_v"] = _[0]
+            if _[0][:-5] == data_relate["data_trace_v"]+'-TraceData':
+                for e in _[1:]:
+                    e_c = e
+                    data_trace_v.append(e_c)
+        print(data_relate)
+
+def add_data_pvt(file_src, file_des):
+    """
+        add data for pvt file
+    """
+    data_trace_v = []
+    get_data_pvt(file_src, data_trace_v)
+    data_trace_cmdv = []
+    for i in data_trace_v:
+        data_trace_cmdv.append(float(i))
+    data_trace_cmdp = []
+    for i in range(len(data_trace_cmdv)):
+        _ = 0
+        for j in data_trace_cmdv[:i]:
+            _ += 0.002*j
+        data_trace_cmdp.append(_)
+    with open(file_des, 'w') as f:
+        f.write('G01 I13=2 PVT2.0\n')
+        length = min(len(data_trace_cmdp), len(data_trace_cmdv))
+        for i in range(length):
+            f.write('X'+str(data_trace_cmdp[i])+':'+str(data_trace_cmdv[i])+' '+'Y0:0'+' '+'Z0:0'+'\n')
     
 def main():
-    set_settings("setting.json", "new_data.dat")
-    add_data("新代规划分析\XD-X-斜边-2ms-v1.std", "new_data.dat")
+    add_data_pvt("XD-X-斜边-2ms-v1.std", "new_data.dat")
     
-
 if __name__ == "__main__":
     main()
 
