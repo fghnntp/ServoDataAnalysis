e0917fc4b3c0aee6198c39758f33b1a56d7f4dff
liu.l.y <fghnntp@hotmial.com>
2020-12-15 14:18:56 +0800

    plot in one figure cimplete

--- realTimeData/main.py
+++ realTimeData/main.py
@@ -41,7 +41,8 @@ class mainWindow(QDialog):
         self.plot_setting = {}
         self.data_chan_num = 0
         self.data_src = None
-        self.plot_locate = [1, 1, 1, 1, 1, 1, 1, 1]
+        self.plot_locate = [[1, 1], [1, 1], [1, 1], [1, 1]]
+        self.plot_info = ['', '', '', '']
         #信号，槽绑定
         self.ui.spinBox_num_set_row.valueChanged.connect(self.plot_row_change)
         self.ui.spinBox_num_set_col.valueChanged.connect(self.plot_col_change)
@@ -54,7 +55,12 @@ class mainWindow(QDialog):
         self.ui.spinBox_data3_col.valueChanged.connect(self.chan3_locate2_change)
         self.ui.spinBox_data4_row.valueChanged.connect(self.chan4_locate1_change)
         self.ui.spinBox_data4_col.valueChanged.connect(self.chan4_locate2_change)
-
+        self.ui.comboBox_data_se1.currentIndexChanged.connect(self.comboBox_data_se1_change)
+        self.ui.comboBox_data_sel2.currentIndexChanged.connect(self.comboBox_data_sel2_change)
+        self.ui.comboBox_data_se3.currentIndexChanged.connect(self.comboBox_data_se3_change)
+        self.ui.comboBox_data_se4.currentIndexChanged.connect(self.comboBox_data_se4_change)
+        self.ui.pushButton_plot.clicked.connect(self.plot_data)
+        
     def plot_row_change(self):
         """
             改变绘制行
@@ -70,28 +76,76 @@ class mainWindow(QDialog):
         self.col = col
     
     def chan1_locate1_change(self):
-        self.plot_locate[0] = self.ui.spinBox_data1_row.value()
+        """
+            通道1数据横坐标位置改变
+        """
+        self.plot_locate[0][0] = self.ui.spinBox_data1_row.value()
 
     def chan1_locate2_change(self):
-        self.plot_locate[1] = self.ui.spinBox_data1_col.value()
+        """
+            通道1数据纵坐标位置改变
+        """
+        self.plot_locate[0][1] = self.ui.spinBox_data1_col.value()
 
     def chan2_locate1_change(self):
-        self.plot_locate[2] = self.ui.spinBox_data2_row.value()
+        """
+            通道2数据横坐标位置改变
+        """
+        self.plot_locate[1][0] = self.ui.spinBox_data2_row.value()
 
     def chan2_locate2_change(self):
-        self.plot_locate[3] = self.ui.spinBox_data2_col.value()
+        """
+            通道2数据纵坐标位置改变
+        """
+        self.plot_locate[1][1] = self.ui.spinBox_data2_col.value()
 
     def chan3_locate1_change(self):
-        self.plot_locate[4] = self.ui.spinBox_data3_row.value()
+        """
+            通道3数据横坐标位置改变
+        """
+        self.plot_locate[2][0] = self.ui.spinBox_data3_row.value()
 
     def chan3_locate2_change(self):
-        self.plot_locate[5] = self.ui.spinBox_data3_col.value()
+        """
+            通道3数据纵坐标位置改变
+        """
+        self.plot_locate[2][1] = self.ui.spinBox_data3_col.value()
 
     def chan4_locate1_change(self):
-        self.plot_locate[6] = self.ui.spinBox_data4_row.value()
+        """
+            通道4数据横坐标改变
+        """
+        self.plot_locate[3][0] = self.ui.spinBox_data4_row.value()
 
     def chan4_locate2_change(self):
-        self.plot_locate[7] = self.ui.spinBox_data4_col.value()
+        """
+            通道4数据纵坐标位置改变
+        """
+        self.plot_locate[3][1] = self.ui.spinBox_data4_col.value()
+
+    def comboBox_data_se1_change(self):
+        """
+            通道1数据名称改变
+        """
+        self.plot_info[0] = self.ui.comboBox_data_se1.currentText()
+
+    def comboBox_data_sel2_change(self):
+        """
+            通道2数据名称改变
+        """
+        self.plot_info[1] = self.ui.comboBox_data_sel2.currentText()
+
+    def comboBox_data_se3_change(self):
+        """
+            通道3数据名称改变
+        """
+        self.plot_info[2] = self.ui.comboBox_data_se3.currentText()
+    
+    def comboBox_data_se4_change(self):
+        """
+            通道4数据名称改变
+        """
+        self.plot_info[3] = self.ui.comboBox_data_se4.currentText() 
 
     def open_file(self):
         """
@@ -109,17 +163,21 @@ class mainWindow(QDialog):
             self.ui.comboBox_data_sel2.clear()
             self.ui.comboBox_data_se3.clear()
             self.ui.comboBox_data_se4.clear()
-            #add new data chan in all comboBox
+            #将comboBox中的所有数据清空，并且将打开文件所获得的数据加入
             for i in range(1, self.data_chan_num+1):
                 self.ui.comboBox_data_se1.addItem(self.plot_setting['ch'+str(i)+'title'])
                 self.ui.comboBox_data_sel2.addItem(self.plot_setting['ch'+str(i)+'title'])
                 self.ui.comboBox_data_se3.addItem(self.plot_setting['ch'+str(i)+'title'])
                 self.ui.comboBox_data_se4.addItem(self.plot_setting['ch'+str(i)+'title'])
-    
+            #为每个comboBox添加一个空白选项
+            self.ui.comboBox_data_se1.addItem('')
+            self.ui.comboBox_data_sel2.addItem('')
+            self.ui.comboBox_data_se3.addItem('')
+            self.ui.comboBox_data_se4.addItem('')
+                
     def plot_data(self):
         """绘制图形"""
-        pass
-        #plot_data_chan(self.row, self.col, self.data_chan_num, self.data_src, self.plot_setting, self.plot_locate)
+        plot_data_chan(self.filename_src, self.row, self.col, self.data_src, self.plot_setting, self.plot_locate, self.plot_info)
           
 if __name__ == "__main__":
     app=QApplication(sys.argv)
--- realTimeData/plot_data.py
+++ realTimeData/plot_data.py
@@ -1,20 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_data_chan(row, col, data_chan_num, data_chans, plot_setting, locate):
+def plot_data_chan(filename, row, col, data_chans, plot_setting, locate, plot_info):
+    #获取合适的横轴长度
+    x = min([len(data) for data in data_chans])
+    sampletime = float(plot_setting['sampletime'])
+    x = [sampletime*i for i in range(x)]
+    count = 0
+    #当全部绘制在一个位置时
     if row == 1 and col == 1:
         fig, axs = plt.subplots()
+        for chn in plot_info:
+            if chn != '':
+                plt.plot(x, data_chans[int(chn[1])-1], label=chn)
+        plt.legend()
+        plt.title(filename)
+        plt.show()
+        return
     else:
         fig, axs = plt.subplots(row, col)
-    x = eval(plot_setting[numbers])
-    sampletime = eval(plot_setting[sampletime])
-    data_chan_
-    x = [sampletime*i for i in x]
+    
+    for i in range(1, row+1):
+        for j in range(1, col+1):
+            for _ in locate:
+                if _[0]==i and _[1]==j:
+                    axs[i-1][j-1].plot(x, data_chans[int(plot_info[count][1])], label=data_chans[count])
+                    count += 1
+    
     for i in range(row):
         for j in range(col):
-            for _ in locate[::2]:
-                if _[0]==i and _[1]==j:
-                    #axs[i][j].plot(x, data_chans[])
-                    pass
+            axs[i][j].legend()
+    plot.show()
 
     
